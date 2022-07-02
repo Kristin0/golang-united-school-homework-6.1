9f9ca1813cb773b3204dfa7ef99a40e064b5c311
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-07-02 16:19:41 +0400

    Inital commit

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang_united_school_homework"
+	//golang_united_school_homework "golang_united_school_homework/shape"
+)
+
+//import "golang_united_school_homework"
+
+func main(){
+	var s golang_united_school_homework.Shape
+	b := golang_united_school_homework.NewBox(2)
+	b.AddShape(s)
+	b.AddShape(s)
+	b.AddShape(s)
+	
+	
+
+}
+
